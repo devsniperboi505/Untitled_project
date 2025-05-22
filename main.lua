385437c804c465e88c215d9cc0d00e576906fa3b
devsniperboi505 <sniperboi505@protonmail.com>
2025-05-22 17:19:57 +0800

    update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,22 @@
-webcmd = require "plugin/lovebird"
-function love.load()
-    love.system.openURL("http://localhose:8000/")
+local console = require "plugin/console"
+love.keyboard.setKeyRepeat(true) -- Not required.
+
+function love.keypressed(key, scancode, isrepeat)
+  console.keypressed(key, scancode, isrepeat)
 end
-function love.load
-  webcmd.whitelist({153.83.158.136})
+
+function love.textinput(text)
+  console.textinput(text)
+end
+
+function love.draw()
+  console.draw()
+end
+
+-- This function is run when the user types "save" into the console.
+console.COMMANDS.save = function()
+  -- Example: save game state
+end
+
+-- This table is available as "player" in the console scope.
+console.ENV.player = {x = 100, y = 5}
